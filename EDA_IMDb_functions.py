1faf0574af79c2dec7eae5f70636b551017afa18
Achraf AFILLOU <achraf_afillou@carrefour.com>
2021-11-28 17:25:22 +0100

    wavfile

--- EDA_IMDb_functions.py
+++ EDA_IMDb_functions.py
@@ -1094,7 +1094,7 @@ def set_features():
     with col1:
         iid = st.selectbox('Choisir un fichier audio', sample.iid_hasbird)
     iid = '6abe6818-52ff-4cc6-8e4a'
-    x, sr = librosa.load(f'data/wav/{iid}.wav')
+    x, sr = librosa.load(f'data/wav_samples/{iid}.wav')
 
 
 def set_data():
